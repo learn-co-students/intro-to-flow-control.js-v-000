7b64310f927574ad17f15ec752213d82dc9e07c9
febbraiod <donedesign1@gmail.com>
2016-04-14 09:31:44 -0400

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,1 +1,44 @@
 'use strict';
+
+function basicTeenager(age){
+  if (age > 12 && age < 20){
+    return "You are a teenager!";
+  }
+}
+
+function teenager(age){
+  if (age > 12 && age < 20){
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
+}
+
+function ageChecker(age){
+  if (age > 12 && age < 20){
+    return "You are a teenager!";
+  } else if (age <= 12){
+    return "You are a kid";
+  } else if (age >= 20) {
+    return "You are a grownup";
+  }
+}
+
+function ternaryTeenager(age){
+  return age > 12 && age < 20 ? "You are a teenager" : "You are not a teenager";
+}
+
+function switchAge(age){
+  switch(true){
+    case (age < 13):
+      return "You have an age";
+      break;
+    case (age > 19):
+      return "You have an age";
+      break;
+    default:
+      return "You are a teenager";
+      break;
+  }
+
+}
