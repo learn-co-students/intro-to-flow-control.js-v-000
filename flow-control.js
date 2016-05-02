2e5185fe41837c823fa1515914c760ef26cf67eb
fiveinfinity <jlachance1@gmail.com>
2016-05-01 20:58:03 -0400

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,1 +1,49 @@
 'use strict';
+
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  }
+}
+
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenager";
+  }
+}
+
+function ageChecker(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+  } else if (age < 13) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
+}
+
+// would not take a ternanry statement.
+function ternaryTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager";
+  } else {
+    return "You are not a teenager";
+  }
+
+  //console.log((age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager");
+}
+
+function switchAge(age) {
+  switch(age) {
+    case 13: return "You are a teenager";
+    case 14: return "You are a teenager";
+    case 15: return "You are a teenager";
+    case 16: return "You are a teenager";
+    case 17: return "You are a teenager";
+    case 18: return "You are a teenager";
+    case 19: return "You are a teenager";
+    default: return "You have an age";
+  }
+}
