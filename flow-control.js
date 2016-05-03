16943d85bf902141b6d7c62068acd20d25331f4e
Brian Holland <brian.holland@proatria.com>
2016-05-03 17:05:01 -0500

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function basicTeenager(age){
+  if (13 <= age && age <= 19){
+    return "You are a teenager!";
+  }
+};
+
+function teenager(age){
+  if (13 <= age && age <= 19){
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
+};
+
+function ageChecker(age){
+  if (13 <= age && age <= 19){
+    return "You are a teenager!";
+  } else if (age < 13){
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
+};
+
+function ternaryTeenager(age){
+  return (13 <= age && age <= 19) ? "You are a teenager" : "You are not a teenager";
+};
+
+function switchAge(age){
+  return (13 <= age && age <= 19) ? "You are a teenager" : "You have an age";
+};
