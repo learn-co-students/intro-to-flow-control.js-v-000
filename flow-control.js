97d9e4dd4dc0a64614ef25c4ec1786f16a22999b
fiveinfinity <jlachance1@gmail.com>
2016-05-10 10:29:16 -0500

    fixed ternary statement

--- flow-control.js
+++ flow-control.js
@@ -24,15 +24,8 @@ function ageChecker(age) {
   }
 }
 
-// would not take a ternanry statement.
 function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19) {
-    return "You are a teenager";
-  } else {
-    return "You are not a teenager";
-  }
-
-  //console.log((age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager");
+  return age > 12 && age < 20 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
