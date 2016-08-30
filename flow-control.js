267330ac2aa41855bb60624784eab7bcad8debe6
Buntry <parkergriep@yahoo.com>
2016-08-29 20:00:31 -0400

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,1 +1,39 @@
-'use strict';
+function basicTeenager (x) {
+	if (x >= 13 && x <= 19) {
+		return 'You are a teenager!';
+	}
+}
+
+function teenager(x){
+	if (x >= 13 && x <= 19) {
+		return 'You are a teenager!';
+	} else {
+		return 'You are not a teenager';
+	}
+}
+
+function ageChecker(x){
+	if(x > 19) {
+		return 'You are a grownup';
+	} else if (x < 13) {
+		return 'You are a kid';
+	} else {
+		return 'You are a teenager!';
+	}
+}
+
+function ternaryTeenager(x){
+	if (x >= 13 && x <= 19) {
+		return 'You are a teenager';
+	} else {
+		return 'You are not a teenager';
+	}
+}
+
+function switchAge(x){
+	if (x >= 13 && x <= 19) {
+		return 'You are a teenager';
+	} else {
+		return 'You have an age';
+	}
+}
