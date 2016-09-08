c7ae85335177672708e782fe518789cfd9bf3fc3
Adam Taitano <adam.taitano@gmail.com>
2016-09-08 17:01:26 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,7 @@ function ageChecker(age) {
 };
 
 function ternaryTeenager(age) {
-  var teen = "You are a teenager"
-  var not = "You are not a teenager"
-  (age < 20 && age > 12 ? "You are a teenager" : "You are not a teenager")
+  return age >= 13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 };
 
 function switchAge(age) {
